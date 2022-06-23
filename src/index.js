e2860e70e7861bc407aa9e9413fc8192bb0ca39a
Aman Sharma <creatorspoint0fficial@gmail.com>
2022-06-23 14:49:35 +0530

    hashmaster

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App className="app" />
+    <HashRouter>
+      <App className="app" />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
